e193b5fe53f01f6173d1b44b9183154c4b5d3b32
Caique Coelho Pinheiro <111666245+0246808642@users.noreply.github.com>
2022-12-17 15:55:10 -0300

    Adicionar arquivos de projeto.

---
+++ Xadrez/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+try
+{
+    PartidaDeXadrez partida = new PartidaDeXadrez();
+
+    while (!partida.terminada)
+    {
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab);
+
+
+        Console.WriteLine();
+        Console.Write("Origem: ");
+        Posicao origem = Tela.lerPosicaoXandrez().toPosicao();
+        Console.Write("Destino: ");
+        Posicao destino = Tela.lerPosicaoXandrez().toPosicao();
+
+        partida.executaMovimento(origem,destino);
+
+    }
+    
+
+   
+}
+catch( TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.ReadLine();
---
+++ Xadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,26 @@
+﻿namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha {  get; set; }
+        public int coluna { get; set; }
+        
+        public Posicao(int linha,int colua)
+        {
+            this.linha = linha;
+            this.coluna = colua;
+        }
+
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return linha
+                + ","
+                + coluna;
+        }
+    }
+}
